ae34059d05e101ec9374fe1fb05fce34acc826bf
梦 <119370987+derammeng@users.noreply.github.com>
2026-02-22 11:44:34 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -758,7 +758,8 @@ export function apply(ctx: Context, config: Config) {
   return
 })
 
-  musicCmd.action(async ({ session, options }: { session: Session; options: any }, keyword: string) => {
+  musicCmd.action(async (argv: any, keyword: string) => {
+  const { session, options } = argv
     if (!keyword) return '请输入歌曲名，如：点歌 周杰伦 晴天'
     const env = getEnvConfig(session)
     await session.send('🔍 搜索中...')
